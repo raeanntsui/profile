b0cd48376116b1cc32bf52af8f5089d10ee670cf
Raeann <raeann.tsui@gmail.com>
2024-08-13 11:32:43 -0700

    users

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,11 @@
 "use client";
 import React from "react";
+import AddToCart from "./AddToCart";
 
 const ProductCard = () => {
   return (
     <div>
-      <button onClick={() => console.log("Click!")}>Add to Cart</button>
+      <AddToCart />
     </div>
   );
 };
